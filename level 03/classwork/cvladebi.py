82e0fc30669e1434c80b2dedb45733df7c570c3e
Saba-soxa <sokhashvilisaba6@gmail.com>
2024-10-20 11:48:17 +0400

    second commit

---
+++ level 03/classwork/cvladebi.py
@@ -0,0 +1,14 @@
+name = "saba"
+Sur_name = "soxashvili"
+Age = 12
+country = "Georgia"
+city = "Tbilisi"
+car = "BMW"
+Hobby = "learning something new"
+color = "black"
+
+
+
+print ("my name is" " " +name)
+print ("my surname is" " "+Sur_name )
+print ("my age is" " "+Age)
