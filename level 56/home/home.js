bb404bdfa04dc3b35cdb44a6503159734f86d4f4
Saba-soxa <sokhashvilisaba6@gmail.com>
2025-04-27 23:47:25 +0400

    second commit

---
+++ level 56/home/home.js
@@ -0,0 +1,17 @@
+// N1
+let Name = 'Saba'
+let Lastname = 'Sokhashvili'
+let age = '13'
+
+// N2
+conts Name = "saba";
+Name = "saba"
+console.log(Name);
+
+conts age = "13";
+age = "13"
+console.log(age);
+
+conts Lastname = "Sokhashvili";
+Lastname = "Sokhahvili"
+console.log(Lastname);
